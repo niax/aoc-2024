85c4e6d08c1f57d52f05ad90198d08594b202c05
Nicholas Hollett <niax@niax.co.uk>
2024-12-02 21:16:36 -0800

    Solve day 3

---
+++ cmd/day03/day03.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	inputFd, err := os.Open("inputs/03")
+	if err != nil {
+		panic(err)
+	}
+	defer inputFd.Close()
+
+	re, _ := regexp.Compile("(mul\\((\\d+),(\\d+)\\)|do\\(\\)|don't\\(\\))")
+	re.Match([]byte("x"))
+
+	p1 := 0
+	p2 := 0
+	input := bufio.NewScanner(inputFd)
+	enable := true
+	for input.Scan() {
+		line := input.Text()
+		res := re.FindAllString(line, -1)
+		for i := range res {
+			submatch := re.FindStringSubmatch(res[i])
+			if submatch[0] == "do()" {
+				enable = true
+			} else if submatch[0] == "don't()" {
+				enable = false
+			}
+			a, _ := strconv.Atoi(submatch[2])
+			b, _ := strconv.Atoi(submatch[3])
+			p1 += (a * b)
+			if enable {
+				p2 += (a * b)
+			}
+		}
+	}
+
+	fmt.Printf("%d\n%d\n", p1, p2)
+}
