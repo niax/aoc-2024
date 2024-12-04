94126c30d70af2e55cf3c9280b1945b55f676cbf
Nicholas Hollett <niax@niax.co.uk>
2024-12-03 23:56:29 -0800

    Maybe make day 4 faster

--- cmd/day04/day04.go
+++ cmd/day04/day04.go
@@ -7,38 +7,55 @@ import (
 	"os"
 )
 
-type charGrid [][]byte
+type SliceGrid[T any] struct {
+	data   []T
+	width  int
+	height int
+}
 
-func (g charGrid) Print() {
-	for y := range g {
-		for x := range g[y] {
-			fmt.Print(string(rune(g[y][x])))
-		}
-		fmt.Print("\n")
+func NewSliceGrid[T any](width, height int) *SliceGrid[T] {
+	data := make([]T, width*height)
+	return &SliceGrid[T]{
+		data:   data,
+		width:  width,
+		height: height,
 	}
 }
 
-func (g charGrid) At(x, y int) (byte, bool) {
-	if y < 0 || y >= len(g) {
-		return 0, false
-	}
-
-	row := g[y]
-	if x < 0 || x >= len(row) {
-		return 0, false
-	}
-
-	return row[x], true
+func (g *SliceGrid[T]) coordIdx(x, y int) int {
+	return y*g.width + x
 }
 
-func (g charGrid) IterDir(x, y, dx, dy int) iter.Seq[byte] {
-	return func(yield func(byte) bool) {
+func (g *SliceGrid[T]) Width() int {
+	return g.width
+}
+
+func (g *SliceGrid[T]) Height() int {
+	return g.height
+}
+
+func (g *SliceGrid[T]) Set(x, y int, v T) {
+	if x < 0 || y < 0 {
+		panic("OOB!")
+	}
+	g.data[g.coordIdx(x, y)] = v
+}
+
+func (g *SliceGrid[T]) At(x, y int) *T {
+	if x < 0 || y < 0 || x >= int(g.width) || y >= int(g.height) {
+		return nil
+	}
+	return &g.data[g.coordIdx(x, y)]
+}
+
+func (g *SliceGrid[T]) IterDir(x, y, dx, dy int) iter.Seq[T] {
+	return func(yield func(T) bool) {
 		for {
-			b, ok := g.At(x, y)
-			if !ok {
+			v := g.At(x, y)
+			if v == nil {
 				return
 			}
-			if !yield(b) {
+			if !yield(*v) {
 				return
 			}
 			x += dx
@@ -55,63 +72,56 @@ func main() {
 	defer inputFd.Close()
 
 	input := bufio.NewScanner(inputFd)
-	grid := make(charGrid, 0, 1024)
+	grid := NewSliceGrid[byte](140, 140)
+	y := 0
 	for input.Scan() {
 		line := input.Text()
-		row := make([]byte, len(line))
-		for i := range line {
-			row[i] = line[i]
+		for x := range line {
+			grid.Set(x, y, line[x])
 		}
-		grid = append(grid, row)
+		y++
 	}
 
 	p1 := 0
 	p2 := 0
-	acc := make([]byte, 4)
-	for y := range grid {
-		for x := range grid[y] {
+	for y := range grid.Height() {
+		for x := range grid.Width() {
 			for dy := -1; dy <= 1; dy++ {
 				for dx := -1; dx <= 1; dx++ {
 					if dx == 0 && dy == 0 {
 						continue
 					}
-					acc = acc[:0]
+					i := 0
 					for c := range grid.IterDir(x, y, dx, dy) {
-						acc = append(acc, c)
-						if len(acc) == 4 {
-							if acc[0] == 88 && acc[1] == 77 && acc[2] == 65 && acc[3] == 83 {
-								p1++
-							}
+						i++
+						if i == 1 && c != 88 {
 							break
+						} else if i == 2 && c != 77 {
+							break
+						} else if i == 3 && c != 65 {
+							break
+						} else if i == 4 && c != 83 {
+							break
+						} else if i > 4 {
+							break
+						} else if i == 4 && c == 83 {
+							p1++
 						}
 					}
 				}
 			}
 
-			if grid[y][x] == 65 {
-				topLeft, ok := grid.At(x-1, y-1)
-				if !ok {
-					goto exit
-				}
-				topRight, ok := grid.At(x+1, y-1)
-				if !ok {
-					goto exit
-				}
-				bottomLeft, ok := grid.At(x-1, y+1)
-				if !ok {
-					goto exit
-				}
-				bottomRight, ok := grid.At(x+1, y+1)
-				if !ok {
-					goto exit
-				}
+			if *grid.At(x, y) == 65 && x > 0 && x < (grid.Width()-1) && y > 0 && y < (grid.Height()-1) {
+				topLeft := *grid.At(x-1, y-1)
+				topRight := *grid.At(x+1, y-1)
+				bottomLeft := *grid.At(x-1, y+1)
+				bottomRight := *grid.At(x+1, y+1)
 				hasTopLeftMas := (topLeft == 77 && bottomRight == 83) || (topLeft == 83 && bottomRight == 77)
 				hasTopRightMas := (topRight == 77 && bottomLeft == 83) || (topRight == 83 && bottomLeft == 77)
 				if hasTopLeftMas && hasTopRightMas {
 					p2++
 				}
 			}
-		exit:
 		}
 	}
 
